2b6db98082f8ed583c897b733f08616aa8b6e589
bansikah22 <tandapnoelbansikah@gmail.com>
2026-08-24 17:11:16 +0100

    Initial Commit

---
+++ PortMedic/PortMedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortMedicApp.swift
+//  PortMedic
+//
+//  Created by bansikah on 24/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct PortMedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
